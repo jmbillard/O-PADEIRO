84ae754a51902263146ec10b35bb87e0fdafb8f1
jmbillard <jmbillard2@gmail.com>
2024-11-05 13:35:10 -0300

    v1.58 novo .jsxbin

--- O_PADEIRO.js
+++ O_PADEIRO.js
@@ -4,7 +4,7 @@
 function O_PADEIRO_UTL(thisObj) {
 	// Declaração da versão do script 'O Padeiro'
 	var scriptName = 'O PADEIRO';
-	var scriptVersion = 'v1.57';
+	var scriptVersion = 'v1.58';
 
 	try {
 		scriptMainPath = scriptMainPath;
