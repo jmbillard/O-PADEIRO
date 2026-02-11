0f0d483767d6205368fcfcfa8c994826a576f036
jmbillard <jmbillard2@gmail.com>
2026-02-11 08:48:26 -0300

    v1.75 melhorias no sistema de rename das comps. agora priorizando o nome das pastas, e depois, os nomes das próprias comps para definir os prefixos.

--- O_PADEIRO.js
+++ O_PADEIRO.js
@@ -4,7 +4,7 @@
 function O_PADEIRO_UTL(thisObj) {
 	// Declaração da versão do script 'O Padeiro'
 	var scriptName = 'O PADEIRO';
-	var scriptVersion = 'v1.74';
+	var scriptVersion = 'v1.75';
 
 	try {
 		scriptMainPath = scriptMainPath;
